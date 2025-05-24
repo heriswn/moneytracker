a60d746ddce1aeb02d622bc75de6e4ced37e1189
헤리 세티아완 <48311762+heriswn@users.noreply.github.com>
2025-05-24 07:45:10 +0000

    update

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get(
 );
 
 app.get(
-  "/auth/google/callback",
+  process.env.GOOGLE_CALLBACK_URL || "/auth/google/callback",
   passport.authenticate("google", {
     failureRedirect: "/",
     successRedirect: "/", // redirect to home
