c08dc250ff73e7b84bdcb01b8d58409a26271244
헤리 세티아완 <48311762+heriswn@users.noreply.github.com>
2025-05-25 22:47:03 +0000

    try solve 10

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: GOOGLE_CALLBACK_URL,
     },
     async (accessToken, refreshToken, profile, done) => {
       let user = await User.findOne({ googleId: profile.id });
